c2bb802cec1c52e6f45d967c58115337addbeeea
Duc Tran <victor.tran.august@gmail.com>
2022-09-17 15:56:05 +0700

    fix: update public url for image

--- src/components/constants.js
+++ src/components/constants.js
@@ -2,9 +2,11 @@ export const DEFAULT_STARS = 5;
 export const DEFAULT_VALUE = 0;
 export const DEFAULT_STEPS = 1;
 
-export const DEFAULT_EMPTY_ICON = '/icons/stars/empty.svg';
-export const DEFAULT_FILLED_ICON = '/icons/stars/filled.svg';
-export const DEFAULT_HALF_FILLED_ICON = '/icons/stars/half.svg';
+const baseUrl = process.env.PUBLIC_URL ?? '';
+
+export const DEFAULT_EMPTY_ICON = baseUrl + '/icons/stars/empty.svg';
+export const DEFAULT_FILLED_ICON = baseUrl + '/icons/stars/filled.svg';
+export const DEFAULT_HALF_FILLED_ICON = baseUrl + '/icons/stars/half.svg';
 
 export const EMPTY_STATE = 'empty';
 export const FILLED_STATE = 'filled';
