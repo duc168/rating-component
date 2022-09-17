38d4e3736a93c2c918978b2a1290464355f3f37d
Github Workflow Action <victor.tran.august@gmail.com>
2022-09-17 08:58:57 +0000

    Deploy

---
+++ precache-manifest.bc27628d70ef30eb7043a6c9ea90a0b4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e95ce415b72715f48b375e4871a9cf1",
+    "url": "/rating-component/index.html"
+  },
+  {
+    "revision": "26ac46c1a64c4d2ac82d",
+    "url": "/rating-component/static/css/4.4932a258.chunk.css"
+  },
+  {
+    "revision": "b29d00700e006165c546",
+    "url": "/rating-component/static/css/main.708ec6d8.chunk.css"
+  },
+  {
+    "revision": "26ac46c1a64c4d2ac82d",
+    "url": "/rating-component/static/js/4.e1cf3e76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rating-component/static/js/4.e1cf3e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1852c8848376e9673f",
+    "url": "/rating-component/static/js/5.b9463c32.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/rating-component/static/js/5.b9463c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67316973af02c68a0526",
+    "url": "/rating-component/static/js/6.a02c9dff.chunk.js"
+  },
+  {
+    "revision": "b29d00700e006165c546",
+    "url": "/rating-component/static/js/main.362b638f.chunk.js"
+  },
+  {
+    "revision": "1b322747315ed53fcfcb",
+    "url": "/rating-component/static/js/runtime-main.406e24e5.js"
+  },
+  {
+    "revision": "d46b64e15a751288a098",
+    "url": "/rating-component/static/js/stencil-h8k-logo_2-entry-js.59cd34dc.chunk.js"
+  },
+  {
+    "revision": "2e31cbfbe3a075c83801",
+    "url": "/rating-component/static/js/stencil-polyfills-css-shim.f64c36e4.chunk.js"
+  }
+]);
